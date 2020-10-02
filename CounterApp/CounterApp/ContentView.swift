f36905e09febadce132007b451f5a5e3f57a851f
saki yamamoto <saki.yamamoto@d2cdot.co.jp>
2020-10-02 16:37:36 +0900

    Second commit

--- CounterApp/CounterApp/ContentView.swift
+++ CounterApp/CounterApp/ContentView.swift
@@ -12,11 +12,21 @@ struct ContentView: View {
     @State var number = 0
     var body: some View {
         VStack {
-            Text("\(number)")
+            ZStack{
+                Image("counter")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("\(number)")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                
+            }
             Button(action: {self.number += 1}) {
                 Text("カウント")
             }
         }
+    }
+    func count(){
+        
     }
 }
 
